68b29cd452a8ce7663bc22c823f872fe587b0ed4
neelakandan <52068913+neelakandanoj@users.noreply.github.com>
2023-12-18 09:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import string
 import pickle
-st.set_option('deprecation.showfileUploaderEncoding',False)
+# st.set_option('deprecation.showfileUploaderEncoding',False)
 model = pickle.load(open('model.pkl','rb'))
 
 
